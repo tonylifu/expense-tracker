6b853db6d35ba05191582e21af03e67b074c8f37
Anthony Ohiero Lifu <tonylifu@outlook.com>
2023-05-15 23:35:58 +0100

    adds expense item card

---
+++ lib/widgets/expense/expense_item.dart
@@ -0,0 +1,52 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  final Expense expense;
+
+  const ExpenseItem({
+    required this.expense,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
+        child: Column(
+          children: [
+            Text(
+              expense.title,
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              children: [
+                Text(
+                  '\$${expense.amount.toStringAsFixed(2)}',
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.alarm),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      expense.date.toString(),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
